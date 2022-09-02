835d76909904e5d254edb687c02e04b1862771c7
artémise 32 <saloukadjijatou@gmail.com>
2022-09-02 14:59:50 +0000

    commite

---
+++ partie1/index.js
@@ -0,0 +1,9 @@
+
+/* for(let element of document.body.childNodes){
+console.log(element)
+} */
+
+/* console.log(document.getElementsByTagName("div"))
+console.log(document.getElementsByClassName("daft")) */
+console.log(document.getElementById("randomlist"))
+
